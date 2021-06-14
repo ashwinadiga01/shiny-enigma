5ffea13406449412352a0784462ed1616e777121
nimit2801 <mnimitsavant@outlook.com>
2021-06-14 17:54:29 +0530

    Slideshow working with tts dynamic messages

--- src/bot.js
+++ src/bot.js
@@ -11,30 +11,18 @@ bot.once('ready', () => {
 bot.on('message', async (message) => {
   try {
     if (message.author.bot) return;
+    if (message.content == 'here') {
+      message.channel.send('lund', { tts: true });
+      return;
+    }
+
     let message1 = await message.channel.send(
       'https://cdn.discordapp.com/attachments/853737763085615154/853739632427794432/insta_nimit.jpg'
     );
+    last = await message.channel.send('start', { tts: true });
 
     await message1.react('◀️');
     await message1.react('▶️');
-    // // const filter = (reaction) =>
-    // //   reaction.emoji.name === '▶️' || reaction.emoji.name === '◀️';
-    // // let collected = await message1.awaitReactions(filter, { time: 10000 });
-    // // console.log(collected);
-    // const filter = (reaction, user) =>
-    //   reaction.emoji.name === '▶️' || user.id === '642898906115473428';
-    // const collector = await message.createReactionCollector(filter, {
-    //   time: 10000,
-    // });
-    // collector.on('collect', (r) => console.log(`Collected ${r.emoji.name}`));
-    // collector.on('dispose', (r) => console.log(`Removed ${r.emoji.name}`));
-    // collector.on('remove', (r) => console.log(`Removed ${r.emoji.name}`));
-    // collector.on('end', (collected) => {
-    //   console.log(`Collected ${collected.size} items`);
-    //   message1.edit(
-    //     'https://tenor.com/view/the-office-ouch-michael-scott-steve-carell-lip-bite-gif-11838665'
-    //   );
-    // });
   } catch (error) {
     console.log(error);
   }
@@ -42,25 +30,34 @@ bot.on('message', async (message) => {
 const new1 = () => {
   return 10;
 };
+let last = {
+  delete: () => {
+    console.log('dummy delete');
+  },
+};
 bot.on('messageReactionAdd', async (reaction, user) => {
   const { name } = reaction.emoji;
   const message = reaction.message;
   try {
     if (user.bot) return;
     console.log(user.id);
-    if (user.id == '637564801832452097') {
+    if (user.id == '637564801832452097' || user.id == '642898906115473428') {
       switch (name) {
         case '◀️':
-          message.edit(
+          await message.edit(
             'https://cdn.discordapp.com/attachments/853737763085615154/853739632427794432/insta_nimit.jpg'
           );
-          reaction.users.remove(user);
+          await last.delete();
+          last = await message.channel.send('back', { tts: true });
+          await reaction.users.remove(user);
           break;
         case '▶️':
-          message.edit(
+          await message.edit(
             'https://tenor.com/view/the-office-ouch-michael-scott-steve-carell-lip-bite-gif-11838665'
           );
-          reaction.users.remove(user);
+          await last.delete();
+          last = await message.channel.send('forward', { tts: true });
+          await reaction.users.remove(user);
           break;
       }
     }
